9987f2706971de154f9d7de7eff141e9036412ec
James Wu <ichangjwu@outlook.com>
2021-11-04 21:03:50 +0900

    completed palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let newString = string.toUpperCase();
+  let array1 = Array.from(newString);
 
-};
+  function isLetter(element) {
+    return (/[a-zA-Z]/).test(element);
+  }
+
+  let filtered = array1.filter(isLetter);
+  let reversedArray = [];
+
+  for (let i = filtered.length - 1; i >= 0; i--) {
+    reversedArray.push(filtered[i]);
+  };
+
+  function issameLetter(element, index) {
+    return element == reversedArray[index];
+  }
+
+  return filtered.every(issameLetter);
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
