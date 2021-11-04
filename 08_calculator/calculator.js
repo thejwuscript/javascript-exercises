dc43e8bd76f70c6acf451dd7075f9ffc9f4b04c6
James Wu <ichangjwu@outlook.com>
2021-11-04 19:08:08 +0900

    completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let answer = array.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+  }, 0)
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let ans = array.reduce(reducer, 1)
+  function reducer(previousV, currentV) {
+    return previousV * currentV;
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0) return 1;
+  let array = [];
+  for (i = 1; num >= i; i++) {
+    array.push(i);
+  }
+  function reducer(previousValue, currentValue) {
+    return previousValue * currentValue;
+  }
+  let ans = array.reduce(reducer, 1)
+  return ans;
 };
 
 // Do not edit below this line
