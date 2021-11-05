ca1fb611b8ec39dfd9bd8b1e9f4a6d21bb319349
James Wu <ichangjwu@outlook.com>
2021-11-05 17:14:02 +0900

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  sequence = [1, 1];
+  for (i = 2; i < num; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
 
+  return sequence[num - 1];
 };
 
 // Do not edit below this line
