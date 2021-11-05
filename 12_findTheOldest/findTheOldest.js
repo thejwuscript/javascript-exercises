7cb8c2f356f476c1021868bb3e3f20303ab18854
James Wu <ichangjwu@outlook.com>
2021-11-05 23:03:54 +0900

    completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
 
-};
+  let oldestperson = array[0];
+  for (i = 0; i < array.length; i++) {
+
+    if (array[i].yearOfDeath === undefined) {
+      let today = new Date();
+      array[i].yearOfDeath = today.getFullYear();
+    }
+
+    let age1 = array[i].yearOfDeath - array[i].yearOfBirth;
+
+    if (age1 > oldestperson.yearOfDeath - oldestperson.yearOfBirth) {
+      oldestperson = array[i];
+    }
+
+  }
+
+  return oldestperson;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
