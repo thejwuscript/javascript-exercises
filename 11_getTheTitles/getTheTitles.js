4624df995daa7650dc8bfe4e8fc1b6d947452d38
James Wu <ichangjwu@outlook.com>
2021-11-05 17:30:42 +0900

    finished getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function (obj) {
 
+  let booktitles = obj.map(function (element) {
+    return element.title;
+  });
+  return booktitles;
 };
 
 // Do not edit below this line
