c2b2766cde3358424768cb4319aa2a8e9ce402b6
James Wu <ichangjwu@outlook.com>
2021-11-06 04:22:25 +0900

    completed caesar exercise

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -3,19 +3,15 @@ for (i = 65; i < 91; i++) {
   let BigLetter = String.fromCharCode(i);
   BigLetterArray.push(BigLetter);
 };
-
 let SmallLetterArray = [];
 for (i = 97; i < 123; i++) {
   let SmallLetter = String.fromCharCode(i);
   SmallLetterArray.push(SmallLetter);
 };
-
 let caesar = function (text, shift) {
-  
   if (shift < -25 || shift > 25) {
     shift = shift % 26;
   };
-
   function Replacement(match) {
     if (BigLetterArray.some((element) => element == match)) {
       let index = BigLetterArray.indexOf(match);
@@ -37,9 +33,7 @@ let caesar = function (text, shift) {
       return SmallLetterArray[index];
     };
   };
-
   return text.replace(/[A-Za-z]/g, Replacement);
-
 };
 
 // Do not edit below this line
